a6e1a3c2de8e45eaa5780f07bc81900695e19d2a
phumiwit <68888298+phumiwit@users.noreply.github.com>
2020-12-29 20:36:22 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import { useState } from "react";
-import { CourseCard } from "./components";
+import React, { useEffect, useState } from "react";
+import { CourseCard } from "./components/CourseCard";
 function App() {
   const grade = ["A", "B+", "B", "C+", "C", "D+", "D", "F", "W"];
   const credit = [1, 2, 3];
 
   const [myCourses, setMyCourse] = useState([]);
-  const [inputData, setInputData] = useState({});
-  const [GPA, setGPA] = useState(4.0);
+  const [inputData, setInputData] = useState([]);
 
-  /**
-   * Calculate the GPA of current courses
-   * @returns the GPA of current courses
-   */
-  function calculateGPA() {
-    // TODO
-  }
+  // TODO: Fix this
+  const CalculateGPA = () => {
+    console.log(myCourses);
+    return 4;
+  };
 
   /**
    * Should be called when a course is to be added to the list.
@@ -24,9 +21,21 @@ function App() {
   function addCourse(event) {
     event.preventDefault();
     // TODO
-
-    // recalculate GPA
-    calculateGPA();
+    const Credit = document.querySelector("#CreditSubject").value;
+    const Grade = document.querySelector("#GradeSubject").value;
+    const Course = document.querySelector("#NameSubject").value;
+    const ID = document.querySelector("#IDSubject").value;
+    setMyCourse([
+      ...myCourses,
+      {
+        Date: Date.now(),
+        key: myCourses.length,
+        NameSubject: Course,
+        IDSubject: ID,
+        GradeSubject: Grade,
+        CreditSubject: Credit
+      }
+    ]);
   }
 
   /**
@@ -36,19 +45,106 @@ function App() {
    */
   function onDeleteCourse(id) {
     // TODO
+    setMyCourse(myCourses.filter((Courses) => Courses.Date !== id));
   }
+  const select = {
+    width: 150,
+    marginRight: 10
+  };
+  const button = {
+    marginLeft: "45%",
+    backgroundColor: "white",
+    padding: 5,
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: "black"
+  };
+  const input = {
+    marginRight: 10,
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: "#ddd"
+  };
+  const font = {
+    fontFamily: "san-serif"
+  };
+  const Input = () => {
+    return (
+      <>
+        <div className="text-center p-3">
+          <input
+            type="text"
+            id="NameSubject"
+            placeholder="ชื่อวิชา"
+            style={input}
+          />
+          <input
+            type="text"
+            id="IDSubject"
+            placeholder="รหัสวิชา"
+            style={input}
+          />
+          <select type="radio" id="CreditSubject" style={input} style={select}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+          <select type="radio" id="GradeSubject" style={input} style={select}>
+            <option value="A">A</option>
+            <option value="B+">B+</option>
+            <option value="B">B</option>
+            <option value="C+">C+</option>
+            <option value="C">C</option>
+            <option value="D+">D+</option>
+            <option value="D">D</option>
+            <option value="F">F</option>
+            <option value="W">W</option>
+          </select>
+          <br />
+          <button type="submit" style={button} onClick={addCourse}>
+            เพิ่ม
+          </button>
+        </div>
+      </>
+    );
+  };
+
+  const Output = (props) => {
+    return (
+      <>
+        <div className="text-center p-3">
+          <label for="GPA">GPA : {props.GPA}</label>
+        </div>
+      </>
+    );
+  };
 
   return (
-    <div className="container mx-auto h-screen">
-      <h1 className="text-center text-4xl p-3 tracking-widest">
+    <div className="container mx-auto h-screen" style={font}>
+      <h1
+        className="text-center text-4xl p-3 tracking-widest"
+        style={{ fontSize: "bold" }}
+      >
         GPA CALCULATOR
       </h1>
       <div className="h-2/3 md:w-2/4 p-3 rounded-lg mx-auto overflow-auto">
-        <h1 className="text-2xl my-3">My courses</h1>
-        {/* TODO display courses */}
+        <h1 className="text-2xl my-3" style={{ fontSize: "bold" }}>
+          My courses
+        </h1>
+        {myCourses.map((Courses) => (
+          <CourseCard
+            key={Courses.key}
+            Name={Courses.NameSubject}
+            ID={Courses.IDSubject}
+            Date={Courses.Date}
+            Grade={Courses.GradeSubject}
+            Credit={Courses.CreditSubject}
+            onCompleteClick={(e) => onDeleteCourse(e)}
+          />
+        ))}
       </div>
-      {/* TODO add course input form */}
-      {/* TODO display calculated GPA */}
+      {<Input />}
+      {<Output GPA={CalculateGPA()} />}
     </div>
   );
 }
--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,4 +1,35 @@
+import React from "react";
+import { findByLabelText, render } from "@testing-library/react";
+
 export const CourseCard = (props) => {
   // TODO: design HTML component that displays a course as a "card" on the webpage.
-  return (null);
+  const styles = {
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: "#ddd",
+    backgroundColor: "white"
+  };
+  const button = {
+    color: "red",
+    float: "right"
+  };
+  return (
+    <div style={styles}>
+      <label for="SubjectID">{props.ID}</label>{" "}
+      <button
+        id="delete"
+        style={button}
+        onClick={() => props.onCompleteClick(props.Date)}
+      >
+        X
+      </button>
+      <br />
+      <label for="SubjectName">{props.Name}</label>
+      <br />
+      <label for="Credit">Credit : {props.Credit}</label>
+      <br />
+      <label for="Grade">Grade : {props.Grade}</label>
+    </div>
+  );
 };
